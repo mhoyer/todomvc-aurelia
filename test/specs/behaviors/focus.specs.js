a6b3fe02202a86eaf62aa5c884da28daa02bfb9f
mhoyer <mhoyer@pixelplastic.de>
2015-02-03 20:08:25 +0100

    added focus behavior to focus the todo item textbox after entering edit mode - sadly testing is not possible under node due to requirement on 'window' and stuff (via aurelia-templating)

---
+++ test/specs/behaviors/focus.specs.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var system = require('../../../jspm_packages/system.src.js');
+             require('../../../config.js');
+
+var sinon = require('sinon');
+var chai = require('chai')
+  , expect = chai.expect
+  , should = chai.should();
+
+describe('Focus behavior', function(){
+  var sut;
+
+  describe('when creating a new instance', function() {
+    it.skip('should simply do', function(done) {
+      // following lines will break with due to import of 'aurelia-templating' (and testing under node I guess):
+      //
+      // Potentially unhandled rejection [2] ReferenceError: Error loading "github:aurelia/templating@0.8.8" at file:./todo-mvc/jspm_packages/github/aurelia/templating@0.8.8.js
+      // Error loading "github:aurelia/templating@0.8.8" from "src/behaviors/focus" at file:./todo-mvc/src/behaviors/focus.js
+      //   window is not defined
+      // at execute (evalmachine.<anonymous>:36:33)
+      // at ensureEvaluated (.\todo-mvc\jspm_packages\system.src.js:714:26)
+      // at ensureEvaluated (.\todo-mvc\jspm_packages\system.src.js:706:11)
+      // at ensureEvaluated (.\todo-mvc\jspm_packages\system.src.js:706:11)
+      // at ensureEvaluated (.\todo-mvc\jspm_packages\system.src.js:706:11)
+      // at ensureEvaluated (.\todo-mvc\jspm_packages\system.src.js:706:11)
+      // at ensureEvaluated (.\todo-mvc\jspm_packages\system.src.js:706:11)
+      // at ensureEvaluated (.\todo-mvc\jspm_packages\system.src.js:706:11)
+      // at ensureEvaluated (.\todo-mvc\jspm_packages\system.src.js:706:11)
+      // at Object.execute (.\todo-mvc\jspm_packages\system.src.js:844:11)
+      system.import('src/behaviors/focus').then(function(module) {
+        sut = new module.App(fakeRouter);
+        sut.should.not.be.null();
+        done();
+      });
+    });
+  });
+
+});
