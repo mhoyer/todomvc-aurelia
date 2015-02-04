65d74c392474849a1b290ace8f388b534055e933
Marcel Hoyer <mhoyer@pixelplastic.de>
2015-02-04 10:36:59 +0100

    updated comment in focus.specs

--- test/specs/behaviors/focus.specs.js
+++ test/specs/behaviors/focus.specs.js
@@ -13,21 +13,21 @@ describe('Focus behavior', function(){
 
   describe('when creating a new instance', function() {
     it.skip('should simply do', function(done) {
-      // following lines will break with due to import of 'aurelia-templating' (and testing under node I guess):
+      // following lines will break due to import of 'aurelia-templating' (and testing under node I guess):
       //
-      // Potentially unhandled rejection [2] ReferenceError: Error loading "github:aurelia/templating@0.8.8" at file:./todo-mvc/jspm_packages/github/aurelia/templating@0.8.8.js
-      // Error loading "github:aurelia/templating@0.8.8" from "src/behaviors/focus" at file:./todo-mvc/src/behaviors/focus.js
+      // Potentially unhandled rejection [2] ReferenceError: Error loading "github:aurelia/templating@0.8.8" at file:~/jspm_packages/github/aurelia/templating@0.8.8.js
+      // Error loading "github:aurelia/templating@0.8.8" from "src/behaviors/focus" at file:~/src/behaviors/focus.js
       //   window is not defined
       // at execute (evalmachine.<anonymous>:36:33)
-      // at ensureEvaluated (.\todo-mvc\jspm_packages\system.src.js:714:26)
-      // at ensureEvaluated (.\todo-mvc\jspm_packages\system.src.js:706:11)
-      // at ensureEvaluated (.\todo-mvc\jspm_packages\system.src.js:706:11)
-      // at ensureEvaluated (.\todo-mvc\jspm_packages\system.src.js:706:11)
-      // at ensureEvaluated (.\todo-mvc\jspm_packages\system.src.js:706:11)
-      // at ensureEvaluated (.\todo-mvc\jspm_packages\system.src.js:706:11)
-      // at ensureEvaluated (.\todo-mvc\jspm_packages\system.src.js:706:11)
-      // at ensureEvaluated (.\todo-mvc\jspm_packages\system.src.js:706:11)
-      // at Object.execute (.\todo-mvc\jspm_packages\system.src.js:844:11)
+      // at ensureEvaluated (~\jspm_packages\system.src.js:714:26)
+      // at ensureEvaluated (~\jspm_packages\system.src.js:706:11)
+      // at ensureEvaluated (~\jspm_packages\system.src.js:706:11)
+      // at ensureEvaluated (~\jspm_packages\system.src.js:706:11)
+      // at ensureEvaluated (~\jspm_packages\system.src.js:706:11)
+      // at ensureEvaluated (~\jspm_packages\system.src.js:706:11)
+      // at ensureEvaluated (~\jspm_packages\system.src.js:706:11)
+      // at ensureEvaluated (~\jspm_packages\system.src.js:706:11)
+      // at Object.execute (~\jspm_packages\system.src.js:844:11)
       system.import('src/behaviors/focus').then(function(module) {
         sut = new module.App(fakeRouter);
         sut.should.not.be.null();
