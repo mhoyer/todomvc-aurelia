7ab849b4401c54e4749a3369a97d956fd8c1c21b
mhoyer <mhoyer@pixelplastic.de>
2015-02-06 20:40:44 +0100

    replaced whole gulpfile.js with require-dir approach directly from https://github.com/aurelia/skeleton-navigation

---
+++ build/paths.js
@@ -0,0 +1,11 @@
+var path = require('path');
+
+module.exports = {
+  source:'src/**/*.js',
+  html:'src/**/*.html',
+  style:'styles/**/*.css',
+  output:'dist/',
+  doc:'./doc',
+  e2eSpecsSrc: 'test/e2e/src/*.js',
+  e2eSpecsDist: 'test/e2e/dist/'
+};
