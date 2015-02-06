48e6d23ebf6c57c47c9b80f11ba3a9fa6e379942
mhoyer <mhoyer@pixelplastic.de>
2015-02-06 21:13:14 +0100

    improved karma.conf.js to use mocha-reporter, hidden Chrome, singleRun

--- karma.conf.js
+++ karma.conf.js
@@ -44,7 +44,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['mocha'],
 
 
     // web server port
@@ -66,11 +66,17 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['HiddenChrome'],
+    customLaunchers: {
+      HiddenChrome: {
+        base: 'Chrome',
+        flags: ['--window-size=400,100', '--window-position=50,-150']
+      }
+    },
 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true
   });
 };
