86e009577e32acfdbb68731e5c68526b68cdee5e
mhoyer <mhoyer@pixelplastic.de>
2015-02-06 21:05:15 +0100

    added npm dependencies for karma-mocha, karma-chai and karma-sinon and adjusted karma.conf.js to use mocha+chai+sinon instead of jasmine

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jspm', 'jasmine'],
+    frameworks: ['jspm', 'mocha', 'chai', 'sinon'],
 
     jspm: {
       // Edit this to your needs
