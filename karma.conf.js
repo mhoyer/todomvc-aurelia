81f8ae61c022ccb66971562d5464da7825741369
Marcel Hoyer <mhoyer@pixelplastic.de>
2015-02-07 15:48:12 +0100

    replaced npm packaged 'karma-sinon' and 'karma-chai' with combining 'karma-sinon-chai' package

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jspm', 'mocha', 'chai', 'sinon'],
+    frameworks: ['jspm', 'mocha', 'sinon-chai'],
 
     jspm: {
       // Edit this to your needs
