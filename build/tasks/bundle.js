a104a7a84ed3185e8aee41b7119d8e123ed30801
Marcel Hoyer <mhoyer@pixelplastic.de>
2015-12-05 15:05:53 +0100

    extracted build\bundles.json (similar to skeleton-navigation approach)

--- build/tasks/bundle.js
+++ build/tasks/bundle.js
@@ -1,46 +1,11 @@
 var gulp = require('gulp');
 var bundler = require('aurelia-bundler');
-
-var aureliaIncludes = [
-  'aurelia-bootstrapper',
-  'aurelia-fetch-client',
-  'aurelia-router',
-  'aurelia-animator-css',
-  'github:aurelia/templating-binding',
-  'github:aurelia/templating-resources',
-  'github:aurelia/templating-router',
-  'github:aurelia/loader-default',
-  'github:aurelia/history-browser',
-  'github:aurelia/logging-console',
-  'github:aurelia/framework',
-  'npm:underscore@1.8.3',
-  'npm:underscore@1.8.3/underscore'
-];
+var bundles = require('../bundles.json');
 
 var config = {
   force: true,
   packagePath: '.',
-  bundles: {
-    "dist/app-build": {
-      includes: [
-        '*',
-        '*.html!text',
-        '*.css!text'
-      ],
-      excludes: aureliaIncludes,
-      options: {
-        inject: true,
-        minify: false
-      }
-    },
-    "dist/aurelia": {
-      includes: aureliaIncludes,
-      options: {
-        inject: true,
-        minify: true
-      }
-    }
-  }
+  bundles: bundles.bundles
 };
 
 gulp.task('bundle', function() {
